9df012ad46b27079574831f16b2dbd01ac51d6a6
Steve Ortiz <steve@ortizaggies.com>
2020-02-07 01:33:30 -0600

    Find org and list accounts

--- main.go
+++ main.go
@@ -1,53 +1,112 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func exitError(err error) {
-	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-	os.Exit(1)
+type programInputs struct {
+	bucket string
 }
 
-func listAtDepth(svc *s3.S3, bucket, prefix *string, depth int) {
-	err := svc.ListObjectsPages(&s3.ListObjectsInput{
-		Bucket:    bucket,
-		Delimiter: aws.String("/"),
-		MaxKeys:   aws.Int64(50),
-		Prefix:    prefix,
-	}, func(result *s3.ListObjectsOutput, _ bool) bool {
-		for _, p := range result.CommonPrefixes {
-			if depth == 1 {
-				fmt.Println(*p.Prefix)
-			} else {
-				listAtDepth(svc, bucket, p.Prefix, depth-1)
-			}
-		}
-		return true
-	})
-	if err != nil {
-		exitError(err)
-	}
+type bucketProcessor struct {
+	svc    *s3.S3
+	bucket string
+	prefix string
+	org    string
 }
 
 func main() {
-	var bucket, prefix string
-	var depth int
-	flag.StringVar(&bucket, "bucket", "", "AWS bucket name")
-	flag.StringVar(&prefix, "prefix", "AWSLogs/", "path prefix")
-	flag.IntVar(&depth, "depth", 1, "depth to list files")
-	flag.Parse()
+	inputs, err := getProgramInputs()
+	checkError(err)
+	processor, err := newBucketProcessor(inputs)
+	checkError(err)
+	err = processor.findOrg()
+	checkError(err)
+	err = processor.findAccounts()
+	checkError(err)
+}
 
+func getProgramInputs() (*programInputs, error) {
+	var result programInputs
+	flag.StringVar(&result.bucket, "bucket", "", "AWS bucket name")
+	flag.Parse()
+	if result.bucket == "" {
+		return nil, errors.New("bucket is a required parameter")
+	}
+	return &result, nil
+}
+
+// bucket_name/prefix_name/AWSLogs/OU-ID/Account-ID/CloudTrail/region/YYYY/MM/DD/file_name.json.gz
+
+func newBucketProcessor(inputs *programInputs) (*bucketProcessor, error) {
+	var result bucketProcessor
+	result.bucket = inputs.bucket
+	result.prefix = "AWSLogs/"
 	sess, err := session.NewSession()
 	if err != nil {
-		exitError(err)
+		return nil, err
 	}
-	svc := s3.New(sess)
-	listAtDepth(svc, aws.String(bucket), aws.String(prefix), depth)
+	result.svc = s3.New(sess)
+	return &result, nil
+}
+
+func (p *bucketProcessor) findOrg() error {
+	foundId := false
+	ids, err := p.listFromBucket(p.prefix)
+	if err != nil {
+		return err
+	}
+	for _, id := range ids {
+		if id[0:2] == "o-" {
+			foundId = true
+			p.prefix += id
+		}
+	}
+	if !foundId {
+		return errors.New("Could not find org id in bucket")
+	}
+	return nil
+}
+
+func (p *bucketProcessor) findAccounts() error {
+	accounts, err := p.listFromBucket(p.prefix)
+	if err != nil {
+		return err
+	}
+	for _, account := range accounts {
+		fmt.Println(p.prefix + account)
+	}
+	return nil
+}
+
+func (p *bucketProcessor) listFromBucket(prefix string) ([]string, error) {
+	var result []string
+	err := p.svc.ListObjectsPages(&s3.ListObjectsInput{
+		Bucket:    &p.bucket,
+		Delimiter: aws.String("/"),
+		MaxKeys:   aws.Int64(50),
+		Prefix:    &prefix,
+	}, func(page *s3.ListObjectsOutput, _ bool) bool {
+		for _, p := range page.CommonPrefixes {
+			withoutPrefix := strings.Replace(*p.Prefix, prefix, "", 1)
+			result = append(result, withoutPrefix)
+		}
+		return true
+	})
+	return result, err
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
